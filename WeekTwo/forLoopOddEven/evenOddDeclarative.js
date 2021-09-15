2ec13ca8fd6ef4f248c573d3bf97440daa0f0ea2
Cal Krug <cal.krug@protonmail.com>
2021-09-15 09:12:51 -0500

    Added forLoopOddEven HW Week 2 Day 4

---
+++ WeekTwo/forLoopOddEven/evenOddDeclarative.js
@@ -0,0 +1,9 @@
+console.log("Write a JavaScript for loop that will iterate from 0 to 99. For each iteration, it will check if the current number is odd or even, and display a message to the screen.");
+for (let x = 0; x<=99; x++) {
+    if (x % 2 == 0) {
+        console.log(x + " is even")
+    }
+    else {
+        console.log(x + " is odd")
+    }
+}
